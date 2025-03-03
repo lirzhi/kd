6333ff502bc3eba7759dfb7fb74f70ca2f76f762
huachuan <huachuan@stu.pku.edu.cn>
2025-03-03 18:57:47 +0800

    fix

--- mutil_agents/memory/specific_review_state.py
+++ mutil_agents/memory/specific_review_state.py
@@ -5,7 +5,7 @@ from typing import List, TypedDict
 class SpecificReviewState(TypedDict):
     task_id: str                    # 任务ID
     content: str                    # 待审内容
-    text: str                       # 文本
+    content_section: str            # 待审章节名称
     review_require_list: List[str]  # 指导原则要求列表
     search_plan_list: List          # 搜索计划列表
     search_list: List               # 搜索结果列表
