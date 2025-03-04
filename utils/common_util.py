c99417a5bca38dd89d5188111e1c6a509c056a7b
huachuan <huachuan@stu.pku.edu.cn>
2025-03-04 16:06:03 +0800

    fix

--- utils/common_util.py
+++ utils/common_util.py
@@ -3,6 +3,8 @@ from functools import wraps
 import json
 import logging
 
+from db import settings
+
 def parallelize_processing(field_to_iterate, result_field, max_workers=3):
     def decorator(func):
         @wraps(func)
@@ -42,3 +44,52 @@ class ResponseMessage:
         }
     def to_json(self):
         return json.dumps(self.to_dict())
+    
+import logging
+from db.dbutils.redis_conn import RedisDB, RedisMsg
+import uuid
+
+
+def produce_handle_info(message):
+    if message is None:
+        logging.info("消息为空！")  
+        return
+    # 发送消息到队列
+    success = RedisDB().queue_product(
+        queue=settings.SVR_QUEUE_NAME, 
+        message=message,
+        exp=3600  # 消息保留时间（秒）
+    )
+    if success:
+        logging.info("消息生产成功！")
+    else:
+        logging.info("消息生产失败！")
+
+def get_handle_info():
+    consumer_name = settings.SVR_CONSUMER_NAME
+    group_name = settings.SVR_CONSUMER_GROUP_NAME
+    queue_name = settings.SVR_QUEUE_NAME
+    # 从队列中读取消息（阻塞式）
+    msg = RedisDB().queue_consumer(
+        queue_name=queue_name,
+        group_name=group_name,
+        consumer_name=consumer_name,
+        msg_id=">"  # ">" 表示只读取新消息
+    )
+    if msg is None:
+        return None
+    message_data = None
+    try:
+        # 处理消息
+        message_data = msg.get_message()
+        
+        # 确认消息（ACK）
+        if msg.ack():
+            logging.info("消息确认成功！")
+        else:
+            logging.info("消息确认失败！")
+
+    except Exception as e:
+        logging.info(f"处理消息失败: {str(e)}")
+    return message_data
+
